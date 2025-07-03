1dbd4c3eedfedeaaf024cdeb8aa8a61f0e4af5c5
Maverick C <115343914+FlueBlan@users.noreply.github.com>
2025-07-03 14:51:45 +0100

    Deck SHuffler
    Didn't work, but now cd has a bug which actually looks like single use instances for cards, just need to get rid of them once they're used

--- Assets/Scripts/Settings/DeckBuilder/CardBP.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-
-public class CardBP : MonoBehaviour
-{
-    [SerializeField] private Image carBG;
-    [SerializeField] private TMP_Text cardCost;
-}
