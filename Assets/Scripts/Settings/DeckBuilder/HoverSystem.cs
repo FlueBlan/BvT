0bfaf6cde25d0c0fee068738322f0e1889afffc1
Maverick C <115343914+FlueBlan@users.noreply.github.com>
2025-07-09 12:32:56 +0100

    Wave Modifications
    Turn based in cards need to be converted to wave base

--- Assets/Scripts/Settings/DeckBuilder/HoverSystem.cs
+++ Assets/Scripts/Settings/DeckBuilder/HoverSystem.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class HoverSystem : Singleton<HoverSystem>
 {
     [SerializeField] private CardView cardViewHover;
+    [SerializeField] private Vector3 hoverOffset = new Vector3(0, 2, 0);
     public void Show(Card card, Vector3 position)
     {
         cardViewHover.gameObject.SetActive(true);
         cardViewHover.Setup(card);
-        cardViewHover.transform.position = position;
+        cardViewHover.transform.position = position + hoverOffset;
     }
 
     public void Hide()
