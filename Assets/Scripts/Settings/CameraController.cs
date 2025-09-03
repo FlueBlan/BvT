2b5be0a672634d2ee39e92ab3775c0035b0258cc
Maverick C <115343914+FlueBlan@users.noreply.github.com>
2025-09-03 13:50:11 +0100

    New Main Level
    Main Level

--- Assets/Scripts/Settings/CameraController.cs
+++ Assets/Scripts/Settings/CameraController.cs
@@ -17,8 +17,7 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown (KeyCode.Escape))
-
-        doMovement = !doMovement;
+            doMovement = !doMovement;
 
         // Weird fix, all the movement keys are invese since the camera is rotated 180 degrees
         // This is a temporary fix, will be fixed later
@@ -26,28 +25,25 @@ public class CameraController : MonoBehaviour
             return;
 
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
-        {
-            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
-        }
-        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
-        {
             transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
-        }
+
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+
         if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
-        {
-            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
-        }
-        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
-        {
             transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
-        }
+
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        Vector3 pos = transform.position;
+        if (scroll != 0f)
+        {
+            Vector3 move = transform.forward * scroll * 1000 * scrollSpeed * Time.deltaTime;
+            Vector3 newPos = transform.position + move;
 
-        pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
-
-        transform.position = pos;
+            newPos.y = Mathf.Clamp(newPos.y, minY, maxY);
+            transform.position = newPos;
+        }
     }
 }
